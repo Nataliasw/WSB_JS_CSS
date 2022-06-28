d31a25899b4c8c0c11d7b5767ddb4cfeb6fb93e2
natal <natalka.swiercz@gmail.com>
2022-06-28 09:24:13 +0200

    Rozwiązanie zadania - Random Facts Api

--- Z2/script.js
+++ Z2/script.js
@@ -46,4 +46,52 @@ function generateJoke() {
         jokeDiv.append(joke);
     })
 }
+/////////////////////////////////////////////////////////////
 
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const categories = document.getElementById("cat");
+    const language = ["en", "de"];
+
+        language.forEach(c => {
+            const option = createOptions(c);
+            categories.append(option);
+        })
+
+});
+
+function createOptions(val) {
+    const option = document.createElement("option");
+    option.value = val;
+    option.innerText = val;
+    return option;
+}
+
+
+
+function createFactDiv(fact) {
+    const div = document.createElement("div");
+    div.innerText = fact;
+    return div;
+}
+
+
+
+function generateFact(){
+    const lang = document.getElementById("cat").value;
+    const factDiv = document.getElementById("fact-div");
+
+    factDiv.innerHTML = "";
+
+    axios.get(`https://uselessfacts.jsph.pl/random.json?language=${lang}`).then(response => {
+
+
+
+        const fact = createFactDiv(response.data.text);
+
+
+        factDiv.append(fact);
+
+    })
+
+}
